a0ecf1c066c73ff12d745aca92c270c9efb5efca
Ep1cfy <69407617+Ep1cfy@users.noreply.github.com>
2026-06-01 13:12:10 -0400

    lastfm functionality v1

---
+++ functions/lastfm.js
@@ -0,0 +1,26 @@
+export async function onRequest(context) {
+	const { env } = context;
+  
+	const apiURL =
+	  "https://ws.audioscrobbler.com/2.0/?" +
+	  new URLSearchParams({
+		method: "user.getrecenttracks",
+		user: env.LASTFM_USER,
+		api_key: env.LASTFM_API_KEY,
+		format: "json",
+		limit: "1",
+	  });
+  
+	const res = await fetch(apiURL);
+	const data = await res.json();
+	const track = data.recenttracks?.track?.[0];
+  
+	return Response.json({
+	  title: track.name,
+	  artist: track.artist?.["#text"],
+	  album: track.album?.["#text"],
+	  image: track.image?.at(-1)?.["#text"],
+	  url: track.url,
+	  nowPlaying: track["@attr"]?.nowplaying === "true",
+	});
+  }
